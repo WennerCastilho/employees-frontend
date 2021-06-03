d110ac9ed7b970b8b1c0bd4c52ee072f5aeb30f2
Wenner Castilho <wennercastilho@gmail.com>
2021-06-03 18:52:33 -0300

    Add case to update list employee

--- src/store/modules/employees/reducer.js
+++ src/store/modules/employees/reducer.js
@@ -6,6 +6,15 @@ export const employeesReducer = (state = [], action) => {
     case "@employees/REMOVE":
       return state.filter((employee) => employee.id !== action.employee.id);
 
+    case "@employees/UPDATE":
+      let employees_update = [
+        ...state.filter((employee) => employee.id !== action.employee.data.id),
+        action.employee.data,
+      ];
+      return employees_update.sort((a, b) =>
+        a.id < b.id ? -1 : a.id > b.id ? 1 : 0
+      );
+
     default:
       return state;
   }
