9db204782a7647a2cefa0435a9a22e8abd8f3175
Wenner Castilho <wennercastilho@gmail.com>
2021-06-02 22:53:29 -0300

    Add routes to home, search and register

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
-export const App = () => <></>;
+import styled from "styled-components";
+import { Sidebar } from "./components/sidebar";
+import { Switch, Route } from "react-router-dom";
+
+import { ListEmployees } from "./components/listEmployees";
+
+export const App = () => (
+  <Container>
+    <Sidebar />
+    <Switch>
+      <Route exact path="/">
+        <ListEmployees />
+      </Route>
+      <Route path="/buscar">Campo de Busca</Route>
+      <Route path="/cadastrar">Página de Cadastro</Route>
+    </Switch>
+  </Container>
+);
+
+export const Container = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 50px;
+  bottom: 50px;
+  left: 50px;
+
+  display: flex;
+
+  background-color: #ffffff4d;
+  border-radius: 5px;
+  font-family: sans-serif;
+  backdrop-filter: blur(10px);
+`;
+// SideBar #00363a
